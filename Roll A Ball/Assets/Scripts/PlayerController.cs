e12109930071466e9df29ff5a2596ff584e438c1
David Koval <kovaldavid@hotmail.com>
2018-02-11 03:04:45 -0600

    rollaball update

--- Roll A Ball/Assets/Scripts/PlayerController.cs
+++ Roll A Ball/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody rb;
 	private int count;
 
+	public Text countText;
     public float speed;
+	public Text winText;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
 		count = 0;
+		SetCountText();
+		winText.text = "";
     }
     void FixedUpdate()
     {
@@ -26,7 +31,14 @@ public class PlayerController : MonoBehaviour {
 		{
 			other.gameObject.SetActive (false);
 			count++;
+			SetCountText();
 		}
 	}
-
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 8) {
+			winText.text = "You Win";
+		}
+	}
 }
