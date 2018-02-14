dc819082271c204729a664c6862e22920f484a99
David Koval <kovaldavid@hotmail.com>
2018-02-14 09:46:20 -0600

    big update

---
+++ MazeRunner/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+    private float distance = 5f;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position + offset;
+        //transform.position = player.transform.position - player.transform.forward * distance;
+        transform.LookAt(player.transform);
+    }
+}
