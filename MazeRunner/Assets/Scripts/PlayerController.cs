9fb26fc7b0592f444d6b79bc90f79283fd85f360
David Koval <kovaldavid@hotmail.com>
2018-03-07 23:22:08 -0600

    MazeRunner Major update

--- MazeRunner/Assets/Scripts/PlayerController.cs
+++ MazeRunner/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody rb;
@@ -17,5 +18,14 @@ public class PlayerController : MonoBehaviour {
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizonal, 0.0f, moveVertical);
         rb.AddForce(movement * speed);
+
     }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Finish"))
+        {
+            SceneManager.LoadScene("Start");
+        }
+    }
+
 }
